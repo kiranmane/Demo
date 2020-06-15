bd74f9431776c66946e37019749d21d21c2771d4
Kiran Mane <kiran.mane@alpha-ict.in>
2020-06-15 17:10:44 +0530

    Test 2 commit

--- app/src/main/java/com/demo/myapplication/MainActivity.java
+++ app/src/main/java/com/demo/myapplication/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("TEST 2");
+        System.out.println("TEST 5");
     }
 }
